d88fb2a23b889904e2a848bba19a0347caf2e5fc
Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>
2024-11-25 12:16:34 +0200

    [common,alerts] Add storage interface
    Signed-off-by: Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>

--- include/aos/common/alerts/alerts.hpp
+++ include/aos/common/alerts/alerts.hpp
@@ -15,6 +15,33 @@ namespace aos {
 namespace alerts {
 
 /**
+ * Storage interface.
+ */
+class StorageItf {
+public:
+    /**
+     * Sets journal cursor.
+     *
+     * @param cursor journal cursor.
+     * @return Error.
+     */
+    virtual Error SetJournalCursor(const String& cursor) = 0;
+
+    /**
+     * Gets journal cursor.
+     *
+     * @param cursor[out] journal cursor.
+     * @return Error.
+     */
+    virtual Error GetJournalCursor(String& cursor) const = 0;
+
+    /**
+     * Destructor.
+     */
+    virtual ~StorageItf() = default;
+};
+
+/**
  * Sender interface.
  */
 class SenderItf {
