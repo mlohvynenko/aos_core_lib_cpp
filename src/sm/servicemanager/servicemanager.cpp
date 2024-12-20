479f4bdc06ba17d9ea0d617fb0538b7422657f42
Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>
2024-12-20 17:48:59 +0200

    [common,image] Pass install base path to install methods
    Signed-off-by: Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>

--- src/sm/servicemanager/servicemanager.cpp
+++ src/sm/servicemanager/servicemanager.cpp
@@ -507,7 +507,8 @@ Error ServiceManager::InstallService(const ServiceInfo& service)
         }
     }
 
-    if (Tie(servicePath, err) = mImageHandler->InstallService(service, archivePath, serviceSpace); !err.IsNone()) {
+    if (Tie(servicePath, err) = mImageHandler->InstallService(archivePath, mConfig.mServicesDir, service, serviceSpace);
+        !err.IsNone()) {
         return AOS_ERROR_WRAP(err);
     }
 
