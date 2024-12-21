35b920346b7caa484c5d1a09d8e0944dc8e32c8e
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2024-12-22 00:19:57 +0200

    [tests,iam] Refactor stubs to have same pattern
    * stubs defined in same header as original interface;
    * stubs defined in same namespace as original interface.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- tests/iam/stubs/certhandlerstub.hpp
+++ tests/iam/stubs/certhandlerstub.hpp
@@ -5,8 +5,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#ifndef AOS_STORAGE_STUB_HPP_
-#define AOS_STORAGE_STUB_HPP_
+#ifndef AOS_CERTHANDLER_STUB_HPP_
+#define AOS_CERTHANDLER_STUB_HPP_
 
 #include "aos/iam/certmodules/certmodule.hpp"
 
