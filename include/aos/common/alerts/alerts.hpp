f6241b2423b9c5b20a7361403bc45afe38711543
Mykola Kobets <mykola_kobets@epam.com>
2024-12-28 06:42:47 +0200

    [alerts] Move StorageItf to aos-core-sm-cpp
    Signed-off-by: Mykola Kobets <mykola_kobets@epam.com>

--- include/aos/common/alerts/alerts.hpp
+++ include/aos/common/alerts/alerts.hpp
@@ -13,33 +13,6 @@
 namespace aos::alerts {
 
 /**
- * Storage interface.
- */
-class StorageItf {
-public:
-    /**
-     * Sets journal cursor.
-     *
-     * @param cursor journal cursor.
-     * @return Error.
-     */
-    virtual Error SetJournalCursor(const String& cursor) = 0;
-
-    /**
-     * Gets journal cursor.
-     *
-     * @param cursor[out] journal cursor.
-     * @return Error.
-     */
-    virtual Error GetJournalCursor(String& cursor) const = 0;
-
-    /**
-     * Destructor.
-     */
-    virtual ~StorageItf() = default;
-};
-
-/**
  * Sender interface.
  */
 class SenderItf {
