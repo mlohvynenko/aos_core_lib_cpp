aoscloud/aos_iam_lib_cpp

[common,tools] Implement duration as a class
Make duration a class to improve type safety and readability.
This change makes it possible to log duration objects
as a string in the ISO8601 duration format.

Signed-off-by: Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>

include/aos/common/tools/timer.hpp
@@ -71,12 +71,12 @@ public:
         sev.sigev_value.sival_ptr = this;
         sev.sigev_notify_function = TimerFunction;
 
-        its.it_value.tv_sec  = interval / Time::cSeconds;
-        its.it_value.tv_nsec = interval % Time::cSeconds;
+        its.it_value.tv_sec  = interval.Count() / Time::cSeconds.Count();
+        its.it_value.tv_nsec = interval.Count() % Time::cSeconds.Count();
 
         if (!mOneShot) {
-            its.it_interval.tv_sec  = interval / Time::cSeconds;
-            its.it_interval.tv_nsec = interval % Time::cSeconds;
+            its.it_interval.tv_sec  = interval.Count() / Time::cSeconds.Count();
+            its.it_interval.tv_nsec = interval.Count() % Time::cSeconds.Count();
         }
 
         auto ret = timer_create(CLOCK_MONOTONIC, &sev, &mTimerID);

src/common/tools/time.cpp
@@ -1,0 +1,95 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "aos/common/tools/time.hpp"
+
+namespace aos {
+
+/***********************************************************************************************************************
+ * Public
+ **********************************************************************************************************************/
+
+StaticString<cTimeStrLen> Duration::ToISO8601String() const
+{
+    if (mDuration == 0) {
+        return "PT0S";
+    }
+
+    StaticString<cTimeStrLen> result = (mDuration < 0) ? "-P" : "P";
+
+    auto total = abs(mDuration);
+    char buffer[16];
+
+    if (auto years = total / Time::cYear.Count(); years > 0) {
+        snprintf(buffer, sizeof(buffer), "%ldY", years);
+
+        result.Append(buffer);
+
+        total %= Time::cYear.Count();
+    }
+
+    if (auto months = total / Time::cMonth.Count(); months > 0) {
+        snprintf(buffer, sizeof(buffer), "%ldM", months);
+
+        result.Append(buffer);
+        total %= Time::cMonth.Count();
+    }
+
+    if (auto weeks = total / Time::cWeek.Count(); weeks > 0) {
+        snprintf(buffer, sizeof(buffer), "%ldW", weeks);
+
+        result.Append(buffer);
+
+        total %= Time::cWeek.Count();
+    }
+
+    if (auto days = total / Time::cDay.Count(); days > 0) {
+        snprintf(buffer, sizeof(buffer), "%ldD", days);
+
+        result.Append(buffer);
+
+        total %= Time::cDay.Count();
+    }
+
+    const auto hours = total / Time::cHours.Count();
+    total %= Time::cHours.Count();
+
+    const auto minutes = total / Time::cMinutes.Count();
+    total %= Time::cMinutes.Count();
+
+    auto seconds = total / Time::cSeconds.Count();
+    total %= Time::cSeconds.Count();
+
+    if (hours || minutes || seconds || total) {
+        result.Append("T");
+
+        if (hours) {
+            snprintf(buffer, sizeof(buffer), "%ldH", hours);
+            result.Append(buffer);
+        }
+
+        if (minutes) {
+            snprintf(buffer, sizeof(buffer), "%ldM", minutes);
+            result.Append(buffer);
+        }
+
+        if (total == 0 && seconds > 0) {
+            snprintf(buffer, sizeof(buffer), "%ldS", seconds);
+            result.Append(buffer);
+        }
+
+        if (total > 0) {
+            const auto rest = static_cast<double>(total) / Time::cSeconds.Count() + static_cast<double>(seconds);
+
+            snprintf(buffer, sizeof(buffer), "%0.9lfS", rest);
+            result.Append(buffer);
+        }
+    }
+
+    return result;
+}
+
+} // namespace aos

tests/sm/layermanager/layermanager_test.cpp
@@ -201,7 +201,7 @@ TEST_F(LayerManagerTest, ExpiredLayersAreClearedOnInit)
     auto config = CreateConfig();
     config.mTTL = Time::cSeconds;
 
-    const auto expiredTTL = Time::Now().Add(-config.mTTL * 2);
+    const auto expiredTTL = Time::Now().Add(-config.mTTL.Count() * 2);
 
     auto cachedAndExpiredLayer  = CreateLayerData("layer1", 1024, LayerStateEnum::eCached, expiredTTL);
     cachedAndExpiredLayer.mPath = CreateExtractedLayerPath("layer1");
@@ -231,12 +231,12 @@ TEST_F(LayerManagerTest, ExpiredLayersAreClearedOnInit)
 TEST_F(LayerManagerTest, ValidateOutdateLayersByTimer)
 {
     auto config                  = CreateConfig();
-    config.mTTL                  = Time::cSeconds / 2;
+    config.mTTL                  = Time::cSeconds.Count() / 2;
     config.mRemoveOutdatedPeriod = Time::cSeconds;
 
     InitTest(config);
 
-    const auto                   expiredTTL     = Time::Now().Add(-config.mTTL * 2);
+    const auto                   expiredTTL     = Time::Now().Add(-config.mTTL.Count() * 2);
     const std::vector<LayerData> expectedLayers = {
         CreateLayerData("layer2", 1024, LayerStateEnum::eCached, Time::Now().Add(config.mTTL * 10)),
         CreateLayerData("layer3", 1024, LayerStateEnum::eActive, expiredTTL),

tests/sm/servicemanager/servicemanager_test.cpp
@@ -97,10 +97,10 @@ protected:
 
 TEST_F(ServiceManagerTest, RemoveOutdatedServicesByTimer)
 {
-    mConfig.mTTL                  = aos::Time::cSeconds / 2;
+    mConfig.mTTL                  = aos::Time::cSeconds.Count() / 2;
     mConfig.mRemoveOutdatedPeriod = aos::Time::cSeconds;
 
-    const auto        expiredTime    = aos::Time::Now().Add(-mConfig.mTTL);
+    const auto        expiredTime    = aos::Time::Now().Add(-mConfig.mTTL.Count());
     const ServiceData expiredService = {
         "service1", "provider1", "1.0.0", "/aos/services/service1", "", expiredTime, ServiceStateEnum::eCached, 0, 0};
     const std::vector<ServiceData> expected = {

mlohvynenko/aos_core_lib_cpp

include/aos/common/tools/timer.hpp
@@ -71,12 +71,12 @@ public:
         sev.sigev_value.sival_ptr = this;
         sev.sigev_notify_function = TimerFunction;
 
-        its.it_value.tv_sec  = interval / Time::cSeconds;
-        its.it_value.tv_nsec = interval % Time::cSeconds;
+        its.it_value.tv_sec  = interval.Nanoseconds() / Time::cSeconds.Nanoseconds();
+        its.it_value.tv_nsec = interval.Nanoseconds() % Time::cSeconds.Nanoseconds();
 
         if (!mOneShot) {
-            its.it_interval.tv_sec  = interval / Time::cSeconds;
-            its.it_interval.tv_nsec = interval % Time::cSeconds;
+            its.it_interval.tv_sec  = interval.Nanoseconds() / Time::cSeconds.Nanoseconds();
+            its.it_interval.tv_nsec = interval.Nanoseconds() % Time::cSeconds.Nanoseconds();
         }
 
         auto ret = timer_create(CLOCK_MONOTONIC, &sev, &mTimerID);

src/common/tools/time.cpp
@@ -1,0 +1,95 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "aos/common/tools/time.hpp"
+
+namespace aos {
+
+/***********************************************************************************************************************
+ * Public
+ **********************************************************************************************************************/
+
+StaticString<cTimeStrLen> Duration::ToISO8601String() const
+{
+    if (mDuration == 0) {
+        return "PT0S";
+    }
+
+    StaticString<cTimeStrLen> result = (mDuration < 0) ? "-P" : "P";
+
+    auto total = abs(mDuration);
+    char buffer[16];
+
+    if (auto years = total / Time::cYear.Nanoseconds(); years > 0) {
+        snprintf(buffer, sizeof(buffer), "%ldY", years);
+
+        result.Append(buffer);
+
+        total %= Time::cYear.Nanoseconds();
+    }
+
+    if (auto months = total / Time::cMonth.Nanoseconds(); months > 0) {
+        snprintf(buffer, sizeof(buffer), "%ldM", months);
+
+        result.Append(buffer);
+        total %= Time::cMonth.Nanoseconds();
+    }
+
+    if (auto weeks = total / Time::cWeek.Nanoseconds(); weeks > 0) {
+        snprintf(buffer, sizeof(buffer), "%ldW", weeks);
+
+        result.Append(buffer);
+
+        total %= Time::cWeek.Nanoseconds();
+    }
+
+    if (auto days = total / Time::cDay.Nanoseconds(); days > 0) {
+        snprintf(buffer, sizeof(buffer), "%ldD", days);
+
+        result.Append(buffer);
+
+        total %= Time::cDay.Nanoseconds();
+    }
+
+    const auto hours = total / Time::cHours.Nanoseconds();
+    total %= Time::cHours.Nanoseconds();
+
+    const auto minutes = total / Time::cMinutes.Nanoseconds();
+    total %= Time::cMinutes.Nanoseconds();
+
+    auto seconds = total / Time::cSeconds.Nanoseconds();
+    total %= Time::cSeconds.Nanoseconds();
+
+    if (hours || minutes || seconds || total) {
+        result.Append("T");
+
+        if (hours) {
+            snprintf(buffer, sizeof(buffer), "%ldH", hours);
+            result.Append(buffer);
+        }
+
+        if (minutes) {
+            snprintf(buffer, sizeof(buffer), "%ldM", minutes);
+            result.Append(buffer);
+        }
+
+        if (total == 0 && seconds > 0) {
+            snprintf(buffer, sizeof(buffer), "%ldS", seconds);
+            result.Append(buffer);
+        }
+
+        if (total > 0) {
+            const auto rest = static_cast<double>(total) / Time::cSeconds.Nanoseconds() + static_cast<double>(seconds);
+
+            snprintf(buffer, sizeof(buffer), "%0.9lfS", rest);
+            result.Append(buffer);
+        }
+    }
+
+    return result;
+}
+
+} // namespace aos

tests/sm/layermanager/layermanager_test.cpp
@@ -201,7 +201,7 @@ TEST_F(LayerManagerTest, ExpiredLayersAreClearedOnInit)
     auto config = CreateConfig();
     config.mTTL = Time::cSeconds;
 
-    const auto expiredTTL = Time::Now().Add(-config.mTTL * 2);
+    const auto expiredTTL = Time::Now().Add(-config.mTTL.Nanoseconds() * 2);
 
     auto cachedAndExpiredLayer  = CreateLayerData("layer1", 1024, LayerStateEnum::eCached, expiredTTL);
     cachedAndExpiredLayer.mPath = CreateExtractedLayerPath("layer1");
@@ -231,12 +231,12 @@ TEST_F(LayerManagerTest, ExpiredLayersAreClearedOnInit)
 TEST_F(LayerManagerTest, ValidateOutdateLayersByTimer)
 {
     auto config                  = CreateConfig();
-    config.mTTL                  = Time::cSeconds / 2;
+    config.mTTL                  = Time::cSeconds.Nanoseconds() / 2;
     config.mRemoveOutdatedPeriod = Time::cSeconds;
 
     InitTest(config);
 
-    const auto                   expiredTTL     = Time::Now().Add(-config.mTTL * 2);
+    const auto                   expiredTTL     = Time::Now().Add(-config.mTTL.Nanoseconds() * 2);
     const std::vector<LayerData> expectedLayers = {
         CreateLayerData("layer2", 1024, LayerStateEnum::eCached, Time::Now().Add(config.mTTL * 10)),
         CreateLayerData("layer3", 1024, LayerStateEnum::eActive, expiredTTL),

tests/sm/servicemanager/servicemanager_test.cpp
@@ -130,10 +130,10 @@ TEST_F(ServiceManagerTest, DamagedServicesAreRemovedOnStart)
 
 TEST_F(ServiceManagerTest, RemoveOutdatedServicesByTimer)
 {
-    mConfig.mTTL                  = aos::Time::cSeconds / 2;
+    mConfig.mTTL                  = aos::Time::cSeconds.Nanoseconds() / 2;
     mConfig.mRemoveOutdatedPeriod = aos::Time::cSeconds;
 
-    const auto        expiredTime    = aos::Time::Now().Add(-mConfig.mTTL);
+    const auto        expiredTime    = aos::Time::Now().Add(-mConfig.mTTL.Nanoseconds());
     const ServiceData expiredService = {
         "service1", "provider1", "1.0.0", "/aos/services/service1", "", expiredTime, ServiceStateEnum::eCached, 0, 0};
     const std::vector<ServiceData> expected = {