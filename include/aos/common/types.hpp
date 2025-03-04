aoscloud/aos_iam_lib_cpp

[sm,layermanager] Return layer statuses on process desired layers
Signed-off-by: Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>

include/aos/common/types.hpp
@@ -669,6 +669,78 @@ struct ServiceStatus {
 using ServiceStatusStaticArray = StaticArray<ServiceStatus, cMaxNumServices>;
 
 /**
+ * Layer status.
+ */
+struct LayerStatus {
+    /**
+     * Default constructor.
+     */
+    LayerStatus() = default;
+
+    /**
+     * Construct a new layer status object
+     *
+     * @param layerID layer ID.
+     * @param digest layer digest.
+     * @param version layer version.
+     * @param status layer status.
+     * @param error layer error.
+     */
+    LayerStatus(const String& layerID, const String& digest, const String& version,
+        ComponentStatus status = ComponentStatusEnum::eUnknown, const Error& error = ErrorEnum::eNone)
+        : mLayerID(layerID)
+        , mDigest(digest)
+        , mVersion(version)
+        , mStatus(status)
+        , mError(error)
+    {
+    }
+
+    /**
+     * Sets error with specified status.
+     *
+     * @param error error.
+     * @param status status.
+     */
+    void SetError(const Error& error, ComponentStatus status = ComponentStatusEnum::eError)
+    {
+        mError  = error;
+        mStatus = status;
+    }
+
+    StaticString<cServiceIDLen>   mLayerID;
+    StaticString<cLayerDigestLen> mDigest;
+    StaticString<cVersionLen>     mVersion;
+    ComponentStatus               mStatus;
+    Error                         mError;
+
+    /**
+     * Compares layer status.
+     *
+     * @param layer layer status to compare.
+     * @return bool.
+     */
+    bool operator==(const LayerStatus& layer) const
+    {
+        return mLayerID == layer.mLayerID && mDigest == layer.mDigest && mVersion == layer.mVersion
+            && mStatus == layer.mStatus && mError == layer.mError;
+    }
+
+    /**
+     * Compares layer status.
+     *
+     * @param layer layer status to compare.
+     * @return bool.
+     */
+    bool operator!=(const LayerStatus& layer) const { return !operator==(layer); }
+};
+
+/**
+ * Layer status static array.
+ */
+using LayerStatusStaticArray = StaticArray<LayerStatus, cMaxNumLayers>;
+
+/**
  * Service info.
  */
 struct ServiceInfo {

tests/sm/layermanager/layermanager_test.cpp
@@ -113,6 +113,7 @@ protected:
         const StaticString<cFilePathLen> archivePath
             = (uriPrefix == "file://") ? layerID : FS::JoinPath(cDownloadDir, layer.mLayerDigest);
 
+        EXPECT_TRUE(mImageHandler.SetCalculateDigestResult(layerPath, layer.mLayerDigest).IsNone());
         EXPECT_TRUE(mImageHandler.SetInstallResult(archivePath, layerPath).IsNone());
 
         return layer;
@@ -292,9 +293,16 @@ TEST_F(LayerManagerTest, ProcessDesiredLayers)
     for (const auto& testCase : testCases) {
 
         auto desiredLayers = CreateAosLayers(testCase.mDesiredLayers);
+        auto layerStatuses = std::make_unique<LayerStatusStaticArray>();
 
-        auto err = mManager.ProcessDesiredLayers(desiredLayers);
+        auto err = mManager.ProcessDesiredLayers(desiredLayers, *layerStatuses);
         ASSERT_TRUE(err.IsNone()) << err.Message();
+
+        if (auto it = layerStatuses->FindIf(
+                [](const auto& status) { return status.mStatus != ComponentStatusEnum::eInstalled; });
+            it != layerStatuses->end()) {
+            FAIL() << "Invalid layer status";
+        }
 
         StaticArray<LayerData, 4> dbLayers;
 
@@ -307,6 +315,37 @@ TEST_F(LayerManagerTest, ProcessDesiredLayers)
             }) != dbLayers.end())
                 << "Layer " << expectedLayer << " is not installed";
         }
+    }
+}
+
+TEST_F(LayerManagerTest, ProcessDesiredLayersOnInvalidLayer)
+{
+    InitTest();
+
+    const auto desiredLayers = CreateAosLayers({"layer1", "layer2", "layer3"});
+    auto       layerStatuses = std::make_unique<LayerStatusStaticArray>();
+
+    ASSERT_TRUE(mManager.ProcessDesiredLayers(desiredLayers, *layerStatuses).IsNone());
+
+    if (auto it
+        = layerStatuses->FindIf([](const auto& status) { return status.mStatus != ComponentStatusEnum::eInstalled; });
+        it != layerStatuses->end() || layerStatuses->Size() != desiredLayers.Size()) {
+        FAIL() << "Invalid layer status";
+    }
+
+    // Change hash of an installed layer
+    mImageHandler.SetCalculateDigestResult(FS::JoinPath(cLayersDir, "sha256", "layer2"), "hash-mismatch");
+
+    layerStatuses->Clear();
+
+    ASSERT_TRUE(mManager.ProcessDesiredLayers(desiredLayers, *layerStatuses).IsNone());
+
+    if (auto it = layerStatuses->FindIf([](const LayerStatus& status) {
+            return status.mStatus == ComponentStatusEnum::eError && status.mError.Is(ErrorEnum::eInvalidChecksum)
+                && status.mDigest == "sha256:layer2";
+        });
+        it == layerStatuses->end()) {
+        FAIL() << "Expected layer status not found";
     }
 }
 

mlohvynenko/aos_core_lib_cpp

include/aos/common/types.hpp
@@ -681,6 +681,78 @@ struct ServiceStatus {
 using ServiceStatusStaticArray = StaticArray<ServiceStatus, cMaxNumServices>;
 
 /**
+ * Layer status.
+ */
+struct LayerStatus {
+    /**
+     * Default constructor.
+     */
+    LayerStatus() = default;
+
+    /**
+     * Construct a new layer status object
+     *
+     * @param layerID layer ID.
+     * @param digest layer digest.
+     * @param version layer version.
+     * @param status layer status.
+     * @param error layer error.
+     */
+    LayerStatus(const String& layerID, const String& digest, const String& version,
+        ItemStatus status = ItemStatusEnum::eUnknown, const Error& error = ErrorEnum::eNone)
+        : mLayerID(layerID)
+        , mDigest(digest)
+        , mVersion(version)
+        , mStatus(status)
+        , mError(error)
+    {
+    }
+
+    /**
+     * Sets error with specified status.
+     *
+     * @param error error.
+     * @param status status.
+     */
+    void SetError(const Error& error, ItemStatus status = ItemStatusEnum::eError)
+    {
+        mError  = error;
+        mStatus = status;
+    }
+
+    StaticString<cServiceIDLen>   mLayerID;
+    StaticString<cLayerDigestLen> mDigest;
+    StaticString<cVersionLen>     mVersion;
+    ItemStatus                    mStatus;
+    Error                         mError;
+
+    /**
+     * Compares layer status.
+     *
+     * @param layer layer status to compare.
+     * @return bool.
+     */
+    bool operator==(const LayerStatus& layer) const
+    {
+        return mLayerID == layer.mLayerID && mDigest == layer.mDigest && mVersion == layer.mVersion
+            && mStatus == layer.mStatus && mError == layer.mError;
+    }
+
+    /**
+     * Compares layer status.
+     *
+     * @param layer layer status to compare.
+     * @return bool.
+     */
+    bool operator!=(const LayerStatus& layer) const { return !operator==(layer); }
+};
+
+/**
+ * Layer status static array.
+ */
+using LayerStatusStaticArray = StaticArray<LayerStatus, cMaxNumLayers>;
+
+/**
  * Service info.
  */
 struct ServiceInfo {

tests/sm/layermanager/layermanager_test.cpp
@@ -113,6 +113,7 @@ protected:
         const StaticString<cFilePathLen> archivePath
             = (uriPrefix == "file://") ? layerID : FS::JoinPath(cDownloadDir, layer.mLayerDigest);
 
+        EXPECT_TRUE(mImageHandler.SetCalculateDigestResult(layerPath, layer.mLayerDigest).IsNone());
         EXPECT_TRUE(mImageHandler.SetInstallResult(archivePath, layerPath).IsNone());
 
         return layer;
@@ -292,9 +293,16 @@ TEST_F(LayerManagerTest, ProcessDesiredLayers)
     for (const auto& testCase : testCases) {
 
         auto desiredLayers = CreateAosLayers(testCase.mDesiredLayers);
+        auto layerStatuses = std::make_unique<LayerStatusStaticArray>();
 
-        auto err = mManager.ProcessDesiredLayers(desiredLayers);
+        auto err = mManager.ProcessDesiredLayers(desiredLayers, *layerStatuses);
         ASSERT_TRUE(err.IsNone()) << err.Message();
+
+        if (auto it
+            = layerStatuses->FindIf([](const auto& status) { return status.mStatus != ItemStatusEnum::eInstalled; });
+            it != layerStatuses->end()) {
+            FAIL() << "Invalid layer status";
+        }
 
         StaticArray<LayerData, 4> dbLayers;
 
@@ -307,6 +315,37 @@ TEST_F(LayerManagerTest, ProcessDesiredLayers)
             }) != dbLayers.end())
                 << "Layer " << expectedLayer << " is not installed";
         }
+    }
+}
+
+TEST_F(LayerManagerTest, ProcessDesiredLayersOnInvalidLayer)
+{
+    InitTest();
+
+    const auto desiredLayers = CreateAosLayers({"layer1", "layer2", "layer3"});
+    auto       layerStatuses = std::make_unique<LayerStatusStaticArray>();
+
+    ASSERT_TRUE(mManager.ProcessDesiredLayers(desiredLayers, *layerStatuses).IsNone());
+
+    if (auto it
+        = layerStatuses->FindIf([](const auto& status) { return status.mStatus != ItemStatusEnum::eInstalled; });
+        it != layerStatuses->end() || layerStatuses->Size() != desiredLayers.Size()) {
+        FAIL() << "Invalid layer status";
+    }
+
+    // Change hash of an installed layer
+    mImageHandler.SetCalculateDigestResult(FS::JoinPath(cLayersDir, "sha256", "layer2"), "hash-mismatch");
+
+    layerStatuses->Clear();
+
+    ASSERT_TRUE(mManager.ProcessDesiredLayers(desiredLayers, *layerStatuses).IsNone());
+
+    if (auto it = layerStatuses->FindIf([](const LayerStatus& status) {
+            return status.mStatus == ItemStatusEnum::eError && status.mError.Is(ErrorEnum::eInvalidChecksum)
+                && status.mDigest == "sha256:layer2";
+        });
+        it == layerStatuses->end()) {
+        FAIL() << "Expected layer status not found";
     }
 }
 