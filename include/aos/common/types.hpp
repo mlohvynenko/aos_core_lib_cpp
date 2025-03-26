aoscloud/aos_iam_lib_cpp

[sm,servicemanager] Return service statuses on process desired services
Signed-off-by: Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>

include/aos/common/types.hpp
@@ -562,9 +562,115 @@ struct InstanceStatus {
 using InstanceStatusStaticArray = StaticArray<InstanceStatus, cMaxNumInstances>;
 
 /**
+ * Component status type.
+ */
+class ComponentStatusType {
+public:
+    enum class Enum {
+        eUnknown,
+        ePending,
+        eDownloading,
+        eDownloaded,
+        eInstalling,
+        eInstalled,
+        eRemoving,
+        eRemoved,
+        eError,
+    };
+
+    static const Array<const char* const> GetStrings()
+    {
+        static const char* const sInstanceRunStateStrings[] = {
+            "unknown",
+            "pending",
+            "downloading",
+            "downloaded",
+            "installing",
+            "installed",
+            "removing",
+            "removed",
+            "error",
+        };
+
+        return Array<const char* const>(sInstanceRunStateStrings, ArraySize(sInstanceRunStateStrings));
+    };
+};
+
+using ComponentStatusEnum = ComponentStatusType::Enum;
+using ComponentStatus     = EnumStringer<ComponentStatusType>;
+
+/**
+ * Service status.
+ */
+struct ServiceStatus {
+    /**
+     * Default constructor.
+     */
+    ServiceStatus() = default;
+
+    /**
+     * Construct a new service status object
+     *
+     * @param serviceID service ID.
+     * @param version service version.
+     * @param status service status.
+     * @param error service error.
+     */
+    ServiceStatus(const String& serviceID, const String& version,
+        ComponentStatus status = ComponentStatusEnum::eUnknown, const Error& error = ErrorEnum::eNone)
+        : mServiceID(serviceID)
+        , mVersion(version)
+        , mStatus(status)
+        , mError(error)
+    {
+    }
+
+    /**
+     * Sets error with specified status.
+     *
+     * @param error error.
+     * @param status status.
+     */
+    void SetError(const Error& error, ComponentStatus status = ComponentStatusEnum::eError)
+    {
+        mError  = error;
+        mStatus = status;
+    }
+
+    StaticString<cServiceIDLen> mServiceID;
+    StaticString<cVersionLen>   mVersion;
+    ComponentStatus             mStatus;
+    Error                       mError;
+
+    /**
+     * Compares service status.
+     *
+     * @param service status to compare.
+     * @return bool.
+     */
+    bool operator==(const ServiceStatus& service) const
+    {
+        return mServiceID == service.mServiceID && mVersion == service.mVersion && mStatus == service.mStatus
+            && mError == service.mError;
+    }
+
+    /**
+     * Compares service status.
+     *
+     * @param service status to compare.
+     * @return bool.
+     */
+    bool operator!=(const ServiceStatus& service) const { return !operator==(service); }
+};
+
+/**
+ * Service status static array.
+ */
+using ServiceStatusStaticArray = StaticArray<ServiceStatus, cMaxNumServices>;
+
+/**
  * Service info.
  */
-
 struct ServiceInfo {
     StaticString<cServiceIDLen>       mServiceID;
     StaticString<cProviderIDLen>      mProviderID;

tests/sm/servicemanager/servicemanager_test.cpp
@@ -80,6 +80,8 @@ protected:
         FS::ClearDir(cTestRootDir);
 
         mImageHandler.Init(mServiceSpaceAllocator);
+
+        EXPECT_CALL(mDownloader, Download).WillRepeatedly(Return(ErrorEnum::eNone));
     }
 
     Config                             mConfig = CreateConfig();
@@ -251,11 +253,26 @@ TEST_F(ServiceManagerTest, ProcessDesiredServices)
 
         for (const auto& service : testItem.mData) {
             mImageHandler.SetInstallResult(FS::JoinPath(cDownloadDir, service.mServiceID), service.mImagePath);
+            mImageHandler.SetCalculateDigestResult(FS::JoinPath(service.mImagePath, "manifest.json"), "sha256:123");
         }
 
-        EXPECT_TRUE(
-            serviceManager.ProcessDesiredServices(Array<ServiceInfo>(testItem.mInfo.data(), testItem.mInfo.size()))
-                .IsNone());
+        auto serviceStatuses = std::make_unique<ServiceStatusStaticArray>();
+
+        EXPECT_TRUE(serviceManager
+                        .ProcessDesiredServices(
+                            Array<ServiceInfo>(testItem.mInfo.data(), testItem.mInfo.size()), *serviceStatuses)
+                        .IsNone());
+
+        ASSERT_EQ(serviceStatuses->Size(), testItem.mData.size()) << "Invalid service status size";
+
+        if (auto it = serviceStatuses->FindIf(
+                [](const auto& status) { return status.mStatus != ComponentStatusEnum::eInstalled; });
+            it != serviceStatuses->end()) {
+            LOG_DBG() << "Found unexpected service status: id" << it->mServiceID << ", version=" << it->mVersion
+                      << ", status=" << it->mStatus << ", err=" << it->mError;
+
+            FAIL() << "Invalid service status";
+        }
 
         ServiceDataStaticArray installedServices;
 
@@ -266,6 +283,63 @@ TEST_F(ServiceManagerTest, ProcessDesiredServices)
                 return CompareServiceData(std::get<0>(tuple), std::get<1>(tuple));
             }),
                 testItem.mData));
+    }
+}
+
+TEST_F(ServiceManagerTest, ProcessDesiredServicesOnInvalidService)
+{
+    ServiceManager serviceManager;
+
+    ASSERT_TRUE(serviceManager
+                    .Init(mConfig, mOCIManager, mDownloader, mStorage, mServiceSpaceAllocator, mDownloadSpaceAllocator,
+                        mImageHandler)
+                    .IsNone());
+
+    const auto testData = std::vector<ServiceInfo> {
+        {"service1", "provider1", "1.0.0", 0, "url", {}, 0},
+        {"service2", "provider2", "1.0.0", 0, "url", {}, 0},
+        {"service3", "provider3", "1.0.0", 0, "url", {}, 0},
+        {"service4", "provider4", "1.0.0", 0, "url", {}, 0},
+    };
+
+    for (const auto& service : testData) {
+        const auto imagePath = FS::JoinPath(cServicesDir, service.mServiceID);
+
+        mImageHandler.SetInstallResult(FS::JoinPath(cDownloadDir, service.mServiceID), imagePath);
+        mImageHandler.SetCalculateDigestResult(FS::JoinPath(imagePath, "manifest.json"), "sha256:123");
+    }
+
+    const auto desiredServices = Array<ServiceInfo>(testData.data(), testData.size());
+    auto       serviceStatuses = std::make_unique<ServiceStatusStaticArray>();
+
+    ASSERT_TRUE(serviceManager.ProcessDesiredServices(desiredServices, *serviceStatuses).IsNone());
+
+    ASSERT_EQ(serviceStatuses->Size(), testData.size()) << "Invalid service status size";
+
+    if (auto it
+        = serviceStatuses->FindIf([](const auto& status) { return status.mStatus != ComponentStatusEnum::eInstalled; });
+        it != serviceStatuses->end()) {
+        LOG_DBG() << "Found unexpected service status: id=" << it->mServiceID << ", version=" << it->mVersion
+                  << ", status=" << it->mStatus << ", err=" << it->mError;
+
+        FAIL() << "Invalid service status";
+    }
+
+    // Change hash of an installed service
+    mImageHandler.SetCalculateDigestResult(FS::JoinPath(cServicesDir, "service2", "manifest.json"), "hash-mismatch");
+
+    serviceStatuses->Clear();
+    ASSERT_TRUE(serviceManager.ProcessDesiredServices(desiredServices, *serviceStatuses).IsNone());
+
+    ASSERT_EQ(serviceStatuses->Size(), testData.size()) << "Invalid service status size";
+
+    if (auto it = serviceStatuses->FindIf([](const ServiceStatus& status) {
+            return status.mServiceID == "service2" && status.mVersion == "1.0.0"
+                && status.mStatus == ComponentStatusEnum::eError && status.mError.Is(ErrorEnum::eInvalidChecksum);
+        });
+        it == serviceStatuses->end()) {
+
+        FAIL() << "Invalid service status";
     }
 }
 

mlohvynenko/aos_core_lib_cpp

include/aos/common/types.hpp
@@ -574,9 +574,115 @@ struct InstanceStatus {
 using InstanceStatusStaticArray = StaticArray<InstanceStatus, cMaxNumInstances>;
 
 /**
+ * Item status type.
+ */
+class ItemStatusType {
+public:
+    enum class Enum {
+        eUnknown,
+        ePending,
+        eDownloading,
+        eDownloaded,
+        eInstalling,
+        eInstalled,
+        eRemoving,
+        eRemoved,
+        eError,
+    };
+
+    static const Array<const char* const> GetStrings()
+    {
+        static const char* const sStrings[] = {
+            "unknown",
+            "pending",
+            "downloading",
+            "downloaded",
+            "installing",
+            "installed",
+            "removing",
+            "removed",
+            "error",
+        };
+
+        return Array<const char* const>(sStrings, ArraySize(sStrings));
+    };
+};
+
+using ItemStatusEnum = ItemStatusType::Enum;
+using ItemStatus     = EnumStringer<ItemStatusType>;
+
+/**
+ * Service status.
+ */
+struct ServiceStatus {
+    /**
+     * Default constructor.
+     */
+    ServiceStatus() = default;
+
+    /**
+     * Construct a new service status object
+     *
+     * @param serviceID service ID.
+     * @param version service version.
+     * @param status service status.
+     * @param error service error.
+     */
+    ServiceStatus(const String& serviceID, const String& version, ItemStatus status = ItemStatusEnum::eUnknown,
+        const Error& error = ErrorEnum::eNone)
+        : mServiceID(serviceID)
+        , mVersion(version)
+        , mStatus(status)
+        , mError(error)
+    {
+    }
+
+    /**
+     * Sets error with specified status.
+     *
+     * @param error error.
+     * @param status status.
+     */
+    void SetError(const Error& error, ItemStatus status = ItemStatusEnum::eError)
+    {
+        mError  = error;
+        mStatus = status;
+    }
+
+    StaticString<cServiceIDLen> mServiceID;
+    StaticString<cVersionLen>   mVersion;
+    ItemStatus                  mStatus;
+    Error                       mError;
+
+    /**
+     * Compares service status.
+     *
+     * @param service status to compare.
+     * @return bool.
+     */
+    bool operator==(const ServiceStatus& service) const
+    {
+        return mServiceID == service.mServiceID && mVersion == service.mVersion && mStatus == service.mStatus
+            && mError == service.mError;
+    }
+
+    /**
+     * Compares service status.
+     *
+     * @param service status to compare.
+     * @return bool.
+     */
+    bool operator!=(const ServiceStatus& service) const { return !operator==(service); }
+};
+
+/**
+ * Service status static array.
+ */
+using ServiceStatusStaticArray = StaticArray<ServiceStatus, cMaxNumServices>;
+
+/**
  * Service info.
  */
-
 struct ServiceInfo {
     StaticString<cServiceIDLen>       mServiceID;
     StaticString<cProviderIDLen>      mProviderID;

tests/sm/servicemanager/servicemanager_test.cpp
@@ -80,6 +80,8 @@ protected:
         FS::ClearDir(cTestRootDir);
 
         mImageHandler.Init(mServiceSpaceAllocator);
+
+        EXPECT_CALL(mDownloader, Download).WillRepeatedly(Return(ErrorEnum::eNone));
     }
 
     Config                             mConfig = CreateConfig();
@@ -283,11 +285,26 @@ TEST_F(ServiceManagerTest, ProcessDesiredServices)
 
         for (const auto& service : testItem.mData) {
             mImageHandler.SetInstallResult(FS::JoinPath(cDownloadDir, service.mServiceID), service.mImagePath);
+            mImageHandler.SetCalculateDigestResult(FS::JoinPath(service.mImagePath, "manifest.json"), "sha256:123");
         }
 
-        EXPECT_TRUE(
-            serviceManager.ProcessDesiredServices(Array<ServiceInfo>(testItem.mInfo.data(), testItem.mInfo.size()))
-                .IsNone());
+        auto serviceStatuses = std::make_unique<ServiceStatusStaticArray>();
+
+        EXPECT_TRUE(serviceManager
+                        .ProcessDesiredServices(
+                            Array<ServiceInfo>(testItem.mInfo.data(), testItem.mInfo.size()), *serviceStatuses)
+                        .IsNone());
+
+        ASSERT_EQ(serviceStatuses->Size(), testItem.mData.size()) << "Invalid service status size";
+
+        if (auto it
+            = serviceStatuses->FindIf([](const auto& status) { return status.mStatus != ItemStatusEnum::eInstalled; });
+            it != serviceStatuses->end()) {
+            LOG_DBG() << "Found unexpected service status: id" << it->mServiceID << ", version=" << it->mVersion
+                      << ", status=" << it->mStatus << ", err=" << it->mError;
+
+            FAIL() << "Invalid service status";
+        }
 
         auto services = std::make_unique<ServiceDataStaticArray>();
 
@@ -298,6 +315,63 @@ TEST_F(ServiceManagerTest, ProcessDesiredServices)
                 return CompareServiceData(std::get<0>(tuple), std::get<1>(tuple));
             }),
                 testItem.mData));
+    }
+}
+
+TEST_F(ServiceManagerTest, ProcessDesiredServicesOnInvalidService)
+{
+    ServiceManager serviceManager;
+
+    ASSERT_TRUE(serviceManager
+                    .Init(mConfig, mOCIManager, mDownloader, mStorage, mServiceSpaceAllocator, mDownloadSpaceAllocator,
+                        mImageHandler)
+                    .IsNone());
+
+    const auto testData = std::vector<ServiceInfo> {
+        {"service1", "provider1", "1.0.0", 0, "url", {}, 0},
+        {"service2", "provider2", "1.0.0", 0, "url", {}, 0},
+        {"service3", "provider3", "1.0.0", 0, "url", {}, 0},
+        {"service4", "provider4", "1.0.0", 0, "url", {}, 0},
+    };
+
+    for (const auto& service : testData) {
+        const auto imagePath = FS::JoinPath(cServicesDir, service.mServiceID);
+
+        mImageHandler.SetInstallResult(FS::JoinPath(cDownloadDir, service.mServiceID), imagePath);
+        mImageHandler.SetCalculateDigestResult(FS::JoinPath(imagePath, "manifest.json"), "sha256:123");
+    }
+
+    const auto desiredServices = Array<ServiceInfo>(testData.data(), testData.size());
+    auto       serviceStatuses = std::make_unique<ServiceStatusStaticArray>();
+
+    ASSERT_TRUE(serviceManager.ProcessDesiredServices(desiredServices, *serviceStatuses).IsNone());
+
+    ASSERT_EQ(serviceStatuses->Size(), testData.size()) << "Invalid service status size";
+
+    if (auto it
+        = serviceStatuses->FindIf([](const auto& status) { return status.mStatus != ItemStatusEnum::eInstalled; });
+        it != serviceStatuses->end()) {
+        LOG_DBG() << "Found unexpected service status: id=" << it->mServiceID << ", version=" << it->mVersion
+                  << ", status=" << it->mStatus << ", err=" << it->mError;
+
+        FAIL() << "Invalid service status";
+    }
+
+    // Change hash of an installed service
+    mImageHandler.SetCalculateDigestResult(FS::JoinPath(cServicesDir, "service2", "manifest.json"), "hash-mismatch");
+
+    serviceStatuses->Clear();
+    ASSERT_TRUE(serviceManager.ProcessDesiredServices(desiredServices, *serviceStatuses).IsNone());
+
+    ASSERT_EQ(serviceStatuses->Size(), testData.size()) << "Invalid service status size";
+
+    if (auto it = serviceStatuses->FindIf([](const ServiceStatus& status) {
+            return status.mServiceID == "service2" && status.mVersion == "1.0.0"
+                && status.mStatus == ItemStatusEnum::eError && status.mError.Is(ErrorEnum::eInvalidChecksum);
+        });
+        it == serviceStatuses->end()) {
+
+        FAIL() << "Invalid service status";
     }
 }
 