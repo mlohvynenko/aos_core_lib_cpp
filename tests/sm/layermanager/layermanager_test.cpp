aoscloud/aos_iam_lib_cpp

[common,tools] Implement duration as a class
Make duration a class to improve type safety and readability.
This change makes it possible to log duration objects
as a string in the ISO8601 duration format.

Signed-off-by: Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>

tests/sm/layermanager/layermanager_test.cpp
@@ -201,7 +201,7 @@ TEST_F(LayerManagerTest, ExpiredLayersAreClearedOnInit)
     auto config = CreateConfig();
     config.mTTL = Time::cSeconds;
 
-    const auto expiredTTL = Time::Now().Add(-config.mTTL * 2);
+    const auto expiredTTL = Time::Now().Add(-config.mTTL.Count() * 2);
 
     auto cachedAndExpiredLayer  = CreateLayerData("layer1", 1024, LayerStateEnum::eCached, expiredTTL);
     cachedAndExpiredLayer.mPath = CreateExtractedLayerPath("layer1");
@@ -231,12 +231,12 @@ TEST_F(LayerManagerTest, ExpiredLayersAreClearedOnInit)
 TEST_F(LayerManagerTest, ValidateOutdateLayersByTimer)
 {
     auto config                  = CreateConfig();
-    config.mTTL                  = Time::cSeconds / 2;
+    config.mTTL                  = Time::cSeconds.Count() / 2;
     config.mRemoveOutdatedPeriod = Time::cSeconds;
 
     InitTest(config);
 
-    const auto                   expiredTTL     = Time::Now().Add(-config.mTTL * 2);
+    const auto                   expiredTTL     = Time::Now().Add(-config.mTTL.Count() * 2);
     const std::vector<LayerData> expectedLayers = {
         CreateLayerData("layer2", 1024, LayerStateEnum::eCached, Time::Now().Add(config.mTTL * 10)),
         CreateLayerData("layer3", 1024, LayerStateEnum::eActive, expiredTTL),

tests/sm/servicemanager/servicemanager_test.cpp
@@ -97,10 +97,10 @@ protected:
 
 TEST_F(ServiceManagerTest, RemoveOutdatedServicesByTimer)
 {
-    mConfig.mTTL                  = aos::Time::cSeconds / 2;
+    mConfig.mTTL                  = aos::Time::cSeconds.Count() / 2;
     mConfig.mRemoveOutdatedPeriod = aos::Time::cSeconds;
 
-    const auto        expiredTime    = aos::Time::Now().Add(-mConfig.mTTL);
+    const auto        expiredTime    = aos::Time::Now().Add(-mConfig.mTTL.Count());
     const ServiceData expiredService = {
         "service1", "provider1", "1.0.0", "/aos/services/service1", "", expiredTime, ServiceStateEnum::eCached, 0, 0};
     const std::vector<ServiceData> expected = {

mlohvynenko/aos_core_lib_cpp

tests/sm/layermanager/layermanager_test.cpp
@@ -202,7 +202,7 @@ TEST_F(LayerManagerTest, ExpiredLayersAreClearedOnInit)
     auto config = CreateConfig();
     config.mTTL = Time::cSeconds;
 
-    const auto expiredTTL = Time::Now().Add(-config.mTTL * 2);
+    const auto expiredTTL = Time::Now().Add(-config.mTTL.Nanoseconds() * 2);
 
     auto cachedAndExpiredLayer  = CreateLayerData("layer1", 1024, LayerStateEnum::eCached, expiredTTL);
     cachedAndExpiredLayer.mPath = CreateExtractedLayerPath("layer1");
@@ -232,12 +232,12 @@ TEST_F(LayerManagerTest, ExpiredLayersAreClearedOnInit)
 TEST_F(LayerManagerTest, ValidateOutdateLayersByTimer)
 {
     auto config                  = CreateConfig();
-    config.mTTL                  = Time::cSeconds / 2;
+    config.mTTL                  = Time::cSeconds.Nanoseconds() / 2;
     config.mRemoveOutdatedPeriod = Time::cSeconds;
 
     InitTest(config);
 
-    const auto                   expiredTTL     = Time::Now().Add(-config.mTTL * 2);
+    const auto                   expiredTTL     = Time::Now().Add(-config.mTTL.Nanoseconds() * 2);
     const std::vector<LayerData> expectedLayers = {
         CreateLayerData("layer2", 1024, LayerStateEnum::eCached, Time::Now().Add(config.mTTL * 10)),
         CreateLayerData("layer3", 1024, LayerStateEnum::eActive, expiredTTL),

tests/sm/servicemanager/servicemanager_test.cpp
@@ -132,10 +132,10 @@ TEST_F(ServiceManagerTest, DamagedServicesAreRemovedOnStart)
 
 TEST_F(ServiceManagerTest, RemoveOutdatedServicesByTimer)
 {
-    mConfig.mTTL                  = aos::Time::cSeconds / 2;
+    mConfig.mTTL                  = aos::Time::cSeconds.Nanoseconds() / 2;
     mConfig.mRemoveOutdatedPeriod = aos::Time::cSeconds;
 
-    const auto        expiredTime    = aos::Time::Now().Add(-mConfig.mTTL);
+    const auto        expiredTime    = aos::Time::Now().Add(-mConfig.mTTL.Nanoseconds());
     const ServiceData expiredService = {
         "service1", "provider1", "1.0.0", "/aos/services/service1", "", expiredTime, ServiceStateEnum::eCached, 0, 0};
     const std::vector<ServiceData> expected = {